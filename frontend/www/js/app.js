2ce24b56c9c75bf806ed3e89732f2489158b43f3
myfucking <myusuf_2870@yahoo.co.id>
2016-05-11 18:26:29 +0700

    Yusuf commit 4

--- frontend/www/js/app.js
+++ frontend/www/js/app.js
@@ -1,6 +1,9 @@
 var app = angular.module('App', [
 	'ionic',
+	'ngMockE2E',
 	'App.routers',
+	'App.services',
+	'App.constants',
 	'App.controllers'
 ])
 
@@ -16,6 +19,38 @@ app.run(function ($ionicPlatform) {
 		if (window.StatusBar) {
 			// org.apache.cordova.statusbar required
 			StatusBar.styleDefault();
+		}
+	});
+})
+
+app.run(function ($httpBackend) {
+	// Don't mock the html views
+	$httpBackend.whenGET(/views\/\w+.*/).passThrough();
+
+	// For everything else, don't mock
+	$httpBackend.whenGET(/^\w+.*/).passThrough();
+	$httpBackend.whenPOST(/^\w+.*/).passThrough();
+})
+
+app.run(function ($rootScope, $state, AuthService, AUTH_EVENTS) {
+	$rootScope.$on('$stateChangeStart', function (event, next, nextParams, fromState) {
+
+		if ('data' in next && 'authorizedRoles' in next.data) {
+			var authorizedRoles = next.data.authorizedRoles;
+			if (!AuthService.isAuthorized(authorizedRoles)) {
+				event.preventDefault();
+				$state.go($state.current, {}, {
+					reload: true
+				});
+				$rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+			}
+		}
+
+		if (!AuthService.isAuthenticated()) {
+			if (next.name !== 'login') {
+				event.preventDefault();
+				$state.go('login');
+			}
 		}
 	});
 })
