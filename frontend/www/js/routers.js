94ef60802b19334c2435e46160e07d9e96e54139
myfucking <myusuf_2870@yahoo.co.id>
2016-05-11 18:27:24 +0700

    Yusuf commit 5

--- frontend/www/js/routers.js
+++ frontend/www/js/routers.js
@@ -1,2 +1,18 @@
 var app = angular.module('App.routers', [])
 
+app.config(function ($stateProvider, $urlRouterProvider) {
+	$stateProvider.state('app', {
+		url: '/app',
+		abstract: true,
+		templateUrl: 'views/partials/menu.html',
+		controller: 'AppCtrl'
+	})
+
+	.state('login', {
+		url: '/login',
+		templateUrl: 'views/pages/login.html',
+		controller: 'LoginCtrl'
+	})
+	
+	$urlRouterProvider.otherwise('/login');
+})
