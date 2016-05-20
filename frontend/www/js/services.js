d874173c5c21aad18cc095675f14e8ee9fcb0e66
irfanmayendra <irfanmayendraputra@gmail.com>
2016-05-20 22:14:48 +0700

    irfan commit 2

--- frontend/www/js/services.js
+++ frontend/www/js/services.js
@@ -6,5 +6,16 @@ app.factory('kategoriService', function ($http) {
 		getAll: function () {
 			return $http.get(baseUrl + 'select_kategori.php');
 		},
+        getIdcustomer: function () {
+			return $http.get(baseUrl + 'select_id_customer.php?customer_username=' + AuthService.username());
+		},
+
+		updatecustomer: function (data) {
+			return $http.post(baseUrl + 'update_customer.php', data, {
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8;'
+				}
+			});
+		}
 	};
 })
